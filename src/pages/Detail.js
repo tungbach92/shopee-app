f08a66dce2a61d2bf7a2e43e11fbbafe19c83a82
tungbach92 <nguyentungbach1992@gmail.com>
2021-06-18 03:49:42 +0700

    hook useParams

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import Header from "../components/Header";
 import HeaderSearch from "../components/HeaderSearch";
 import DetailProduct from "../components/DetailProduct";
 export default function Detail(props) {
-  const { metaTitle } = props.match.params;
+  // const { metaTitle } = props.match.params;
+  const { metaTitle } = useParams();
   return (
     <>
       <Header headerSearch={<HeaderSearch />}></Header>
