31ad9a72ddc81d019398d78e10ad227af18541de
tungbach92 <nguyentungbach1992@gmail.com>
2021-08-02 04:34:11 +0700

    deploy cloud functions to firebase, replace endpoint url

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/shopee-demo-c6d2b/us-central1/api", // api url(our cloud func)
+  baseURL: "https://us-central1-shopee-demo-c6d2b.cloudfunctions.net/api", // api url(our cloud func)
+  //http://localhost:5001/shopee-demo-c6d2b/us-central1/api
 });
 export default instance;
